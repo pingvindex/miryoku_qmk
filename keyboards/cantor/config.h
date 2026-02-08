056b38e3b1c87b6b0d032bdc43cc6e39c8178ae0
Damir Zhadikov <damirzhadikov@Damirs-MacBook-Pro-2.local>
2026-02-08 13:36:43 +0500

    add two more layers and VIA support to cantor

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -7,6 +7,9 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define VIAL_ENABLE 1
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
